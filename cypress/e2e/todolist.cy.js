a5d4ac2d74d4e3e04c77b9473beeb32491b39cbe
Maria Carolina <mcarolina1950@hotmail.com>
2023-11-13 12:18:51 +0000

    test: update tests to pick first list item

--- cypress/e2e/todolist.cy.js
+++ cypress/e2e/todolist.cy.js
@@ -1,6 +1,6 @@
 describe('TodoList', () => {
   beforeEach(function() {
-    cy.visit('http://localhost:8080/index.html?profile=Responsive')
+    cy.visit('https://todolist210-sandbox.mxapps.io/')
   })
 
   it('adds a task to the list', () => {
@@ -8,29 +8,29 @@ describe('TodoList', () => {
     const task = "Do the dishes" + now
 
     cy.get('[data-test="add-task-input"] input').type(task).type("{enter}")
-    cy.get("li:last-child").contains(task)
+    cy.get("li:first-child").contains(task)
   })
 
   it('marks the task as done', function() {
-    const switchElement = cy.get("li:last-child").find('[role="switch"]')
+    const switchElement = cy.get("li:first-child").find('[role="switch"]')
     switchElement.click()
     switchElement.should("have.attr", 'aria-checked', 'true')
   })
 
   it('edits the task', function() {
-    cy.get("li:last-child").find('[data-test= "edit-task-button"] > button').click()
+    cy.get("li:first-child").find('[data-test= "edit-task-button"] > button').click()
     cy.get('[data-test="type-new-task"]').clear().type("wash the plants")
     cy.get('[type="radio"]').first().check()
     cy.get('[data-test= "save-edit-button"] > button').click()
 
-    cy.get("li:last-child").contains("wash the plants")
+    cy.get("li:first-child").contains("wash the plants")
 
-    const switchElement = cy.get("li:last-child").find('[role="switch"]')
+    const switchElement = cy.get("li:first-child").find('[role="switch"]')
     switchElement.should("have.attr", 'aria-checked', 'true')
   })
 
   it('deletes the task', () => {
-    cy.get("li:last-child").find('[data-test="delete-task-button"] > button').click()
+    cy.get("li:first-child").find('[data-test="delete-task-button"] > button').click()
     cy.get("button").contains("OK").click()
   })
 })
